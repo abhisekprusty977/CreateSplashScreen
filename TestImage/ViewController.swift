72afd9891d9ce3bd15c81108917cb0659ef7b013
abhisekprusty977 <abhisekprusty977@gmail.com>
2026-02-14 23:47:37 +0530

    Create Splash Screen on Storyboard

---
+++ TestImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestImage
+//
+//  Created by Abhisek Prusty on 14/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
